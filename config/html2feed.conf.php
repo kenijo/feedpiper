473e1e7b284dc49b1834f8cf8719be149e9ccf5b
Kenijo <kenrick.jorus@gmail.com>
2017-11-28 23:03:07 -0700

    Updated text

--- config/html2feed.conf.php
+++ config/html2feed.conf.php
@@ -14,12 +14,12 @@
 * You can debug the application by using
 *   http://domain/html2feed.php?page=example&debug=true
 */
-$cfg['example']['page_title']  = 'HTML2FEED Example - GitHub Guides';
-$cfg['example']['page_url']    = 'https://guides.github.com/';
-$cfg['example']['entry']       = 'article[class="guide-listing"]';
-$cfg['example']['title']       = 'h3[class="guide-cover-title"]';
-$cfg['example']['description'] = 'p[class="guide-summary"]';
-$cfg['example']['link']        = 'a';
-$cfg['example']['author']      = null;
-$cfg['example']['category']    = null;
-$cfg['example']['thumbnail']   = null;
+// $cfg['example']['page_title']  = 'HTML2FEED Example - GitHub Guides';
+// $cfg['example']['page_url']    = 'https://guides.github.com/';
+// $cfg['example']['entry']       = 'article[class="guide-listing"]';
+// $cfg['example']['title']       = 'h3[class="guide-cover-title"]';
+// $cfg['example']['description'] = 'p[class="guide-summary"]';
+// $cfg['example']['link']        = 'a';
+// $cfg['example']['author']      = null;
+// $cfg['example']['category']    = null;
+// $cfg['example']['thumbnail']   = null;
