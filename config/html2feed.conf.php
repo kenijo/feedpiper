a8e21749423f776820caec2447755e9be278a518
Kenijo <kenijo@users.noreply.github.com>
2024-10-28 09:40:07 -0700

    Restructure config files for better readability and maintenance

--- config/html2feed.conf.php
+++ config/html2feed.conf.php
@@ -2,24 +2,23 @@
 
 /**
  * HTML2FEED Configuration
- *
  * Used to turn a page into a feed.
- */
-
-/**
- * NOTE: Example with GitHub Guides
  *
  * Configure the application below and call it with
  *   https://DOMAIN/html2feed.php?page=feed_name
  * You can debug the application by using
  *   https://DOMAIN/html2feed.php?page=feed_name&debug=true
  */
-$html2feed['feed_name']['page_title']  = 'HTML2FEED Example - GitHub Guides';
-$html2feed['feed_name']['page_url']    = 'https://guides.github.com/';
-$html2feed['feed_name']['entry']       = 'article[class="guide-listing"]';
-$html2feed['feed_name']['title']       = 'h3[class="guide-cover-title"]';
-$html2feed['feed_name']['description'] = 'p[class="guide-summary"]';
-$html2feed['feed_name']['link']        = 'a';
-$html2feed['feed_name']['author']      = null;
-$html2feed['feed_name']['category']    = null;
-$html2feed['feed_name']['thumbnail']   = null;
+$html2feed = [
+    'feed' => [
+        'page_title'  => 'Another Feed Example',
+        'page_url'    => 'https://example.com/',
+        'entry'       => 'div.entry',
+        'title'       => 'h2.title',
+        'description' => 'div.summary',
+        'link'        => 'a.read-more',
+        'author'      => 'span.author',
+        'category'    => 'span.category',
+        'thumbnail'   => 'img.thumbnail',
+    ],
+];
