b69232c230230d5b65afff7c4606d343cc794be6
Kenijo <kenijo@users.noreply.github.com>
2025-03-12 15:20:22 -0700

    Reorganize and consolidate config file

--- config/feedfilter.conf.original.php
+++ config/feedfilter.conf.original.php
@@ -1,6 +1,13 @@
 <?php
 
 /**
+ * Force SimplePie to use fsockopen() instead of cURL
+ * If cURL doesn't work, set variable to false to use fsockopen()
+ * Default value is true
+ */
+$useCurl = true;
+
+/**
  * ATOM Filter Configuration
  *
  * Used to merge feeds together and output a single feed.
@@ -26,55 +33,50 @@
  *   https://DOMAIN/feedfilter.php?feed=feed_name&debug=true&entry=1
  */
 
-// Filter out if there is no text summary
-//if (!preg_match('//'.'[a-z]+'.'//imu', strip_tags($summary)) || $summary == null)
-
-$feedfilter = [
+$feedConf = [
     // Global filters, works as an anti-spam
     // Skip any feed that contains the following keywords
-    // Blacklist: exclude entries matching the rule (no global whitelisting)
-    'global' => [
-        'blacklist' => [
-            'author' => [
-                'starts' => ['keyword', 'excludes', 'entry'],
-                'contains' => ['keyword', 'excludes', 'entry'],
-                'ends' => ['keyword', 'excludes', 'entry'],
-                'regex' => ['keyword', 'excludes', 'entry'],
-            ],
-            'category' => [
-                'starts' => ['keyword', 'excludes', 'entry'],
-                'contains' => ['keyword', 'excludes', 'entry'],
-                'ends' => ['keyword', 'excludes', 'entry'],
-                'regex' => ['keyword', 'excludes', 'entry'],
-            ],
-            'content' => [
-                'starts' => ['keyword', 'excludes', 'entry'],
-                'contains' => ['keyword', 'excludes', 'entry'],
-                'ends' => ['keyword', 'excludes', 'entry'],
-                'regex' => ['keyword', 'excludes', 'entry'],
-            ],
-            'title' => [
-                'starts' => ['keyword', 'excludes', 'entry'],
-                'contains' => ['keyword', 'excludes', 'entry'],
-                'ends' => ['keyword', 'excludes', 'entry'],
-                'regex' => ['keyword', 'excludes', 'entry'],
-            ],
+    // Blacklist: exclude entries matching the rule (no global whitelist)
+    'globalBlacklist' => [
+        'author' => [
+            'starts' => ['keyword', 'excludes', 'entry'],
+            'contains' => ['keyword', 'excludes', 'entry'],
+            'ends' => ['keyword', 'excludes', 'entry'],
+            'regex' => ['keyword', 'excludes', 'entry'],
+        ],
+        'category' => [
+            'starts' => ['keyword', 'excludes', 'entry'],
+            'contains' => ['keyword', 'excludes', 'entry'],
+            'ends' => ['keyword', 'excludes', 'entry'],
+            'regex' => ['keyword', 'excludes', 'entry'],
+        ],
+        'content' => [
+            'starts' => ['keyword', 'excludes', 'entry'],
+            'contains' => ['keyword', 'excludes', 'entry'],
+            'ends' => ['keyword', 'excludes', 'entry'],
+            'regex' => ['keyword', 'excludes', 'entry'],
+        ],
+        'title' => [
+            'starts' => ['keyword', 'excludes', 'entry'],
+            'contains' => ['keyword', 'excludes', 'entry'],
+            'ends' => ['keyword', 'excludes', 'entry'],
+            'regex' => ['keyword', 'excludes', 'entry'],
         ],
     ],
+
     'feed_name' => [
         'title' => 'Feedpiper Example - GitHub Trends',
         'url' => [
             'http://github-trends.ryotarai.info/rss/github_trends_all_daily.rss',
         ],
-        // Whitelist: keep only entries matching the rules (executed before blacklisting)
-        // NOTE: At this time, only one whitelist can be used per feed (code needs to be reworked to handle multiple whitelists)
+        // Whitelist: never exclude an entry matching the rules (executed before blacklisting)
         'whitelist' => [
             'author' => ['contains' => ['keyword', 'keeps', 'entry']],
             'category' => ['contains' => ['keyword', 'keeps', 'entry']],
             'content' => ['contains' => ['keyword', 'keeps', 'entry']],
             'title' => ['contains' => ['keyword', 'keeps', 'entry']],
         ],
-        // Blacklist: exclude entries matching the rules (executed after whitelisting)
+        // Blacklist: always exclude an entry matching the rules (executed after whitelisting if no whitelist match was found)
         'blacklist' => [
             'author' => [
                 'starts' => ['keyword', 'excludes', 'entry'],
